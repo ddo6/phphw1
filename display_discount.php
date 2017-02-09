2b63bc97576694b2e2ec6ad71d960d4e7f82e3ba
ddo6 <ddo6@njit.edu>
2017-02-08 19:33:07 -0500

    Update display_discount.php

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,17 @@
+<?
+$product_description = $_POST['product_description'];
+$list_price = $_POST['list_price'];
+$discount_percent = $_POST['discount_percent'];
+$
+
+$discount = $list_price * $discount_percent * .01;
+$discount_price = $list_price - $discount;
+
+$list_price_formatted = "$".number_format($list_price, 2);
+$discount_percent_formatted = $discount_percent."%";
+$discount_formatted = "$".number_format($discount, 2);
+$discount_price_formatted = "$".number_format($discount_price, 2);  
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,19 +23,19 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_percent; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price; ?></span><br>
     </main>
 </body>
 </html>
